d3cb391a75c26a5fba5b1afafe84076b57c00a8c
花菱小由 <honaka@MacBookPro.lan>
2026-01-20 23:35:52 -0700

    Finished participation part

--- code/app/src/main/java/com/example/listycity/AddCityFragment.java
+++ code/app/src/main/java/com/example/listycity/AddCityFragment.java
@@ -1,4 +1,90 @@
 package com.example.listycity;
 
-public class AddCityFragment {
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+public class AddCityFragment extends DialogFragment {
+    interface AddCityDialogListener {
+        void addCity(City city);
+
+        void editCity(int position, City updatedCity);
+    }
+
+    private AddCityDialogListener listener;
+
+    private static final String ARG_CITY = "city";
+    private static final String ARG_POS = "position";
+
+    public static AddCityFragment newInstance(City city, int position) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CITY, city);
+        args.putInt(ARG_POS, position);
+
+        AddCityFragment fragment = new AddCityFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof AddCityDialogListener) {
+            listener = (AddCityDialogListener) context;
+        } else {
+            throw new RuntimeException(context + " must implement AddCityDialogListener");
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+
+
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+
+        Bundle args = getArguments();
+        boolean isEdit = (args != null && args.containsKey(ARG_CITY) && args.containsKey(ARG_POS));
+
+        int position = -1;
+        if (isEdit) {
+            City city = (City) args.getSerializable(ARG_CITY);
+            position = args.getInt(ARG_POS);
+
+            if (city != null) {
+                editCityName.setText(city.getName());
+                editProvinceName.setText(city.getProvince());
+            }
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        int finalPosition = position;
+
+
+        return builder
+                .setView(view)
+                .setTitle(isEdit ? "Add/edit city" : "Add a city")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton(isEdit ? "OK" : "Add", (dialog, which) -> {
+                    String cityName = editCityName.getText().toString();
+                    String provinceName = editProvinceName.getText().toString();
+
+                    if (isEdit) {
+                        listener.editCity(finalPosition, new City(cityName, provinceName));
+                    } else {
+                        listener.addCity(new City(cityName, provinceName));
+                    }
+                })
+                .create();
+    }
 }
--- code/app/src/main/java/com/example/listycity/City.java
+++ code/app/src/main/java/com/example/listycity/City.java
@@ -1,4 +1,30 @@
 package com.example.listycity;
+import java.io.Serializable;
 
-public class City {
+public class City implements Serializable {
+    private String name;
+    private String province;
+
+    public City(String name, String province) {
+        this.name = name;
+        this.province = province;
+    }
+    public String getName(){
+
+        return name;
+    }
+
+    public String getProvince(){
+
+        return province;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setProvince(String province){
+        this.province = province;
+    }
+
+
 }
